9516b1f4f6056006a22b0f9b679e3d2098c4d464
Artem Belyi <abelyi467@gmail.com>
2026-05-07 16:21:50 +0300

    installed dotenv created .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD")
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000)
