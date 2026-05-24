15423f3277b8efc9ab7e33dcad77f59a8264434c
Artem Belyi <abelyi467@gmail.com>
2026-05-25 02:30:31 +0300

    remoed user middleware

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { validateUser } from "../middlewares/user";
 import UserController from "../controllers/user";
 
 const router = Router();
 
 router.get("/users",  UserController.getAllUsers);
 
-router.post("/users", [validateUser], UserController.createUser);
+router.post("/users", UserController.createUser);
 
 export default router;
