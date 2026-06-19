8f7a5d3e3464f880cfe8497b266e93361d43daed
Artem Belyi <abelyi467@gmail.com>
2026-06-19 13:20:27 +0300

    created login component

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormRoot, FormField, form, required } from '@angular/forms/signals';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+
+interface LoginData {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormRoot, MatFormField, MatLabel, MatInput, MatButton, FormField],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  readonly loginModel = signal<LoginData>({
+    username: "",
+    password: "",
+  });
+
+  readonly loginForm = form<LoginData>(
+    this.loginModel,
+    (schemaPath) => {
+      required(schemaPath.username, { message: 'Username is required' });
+      required(schemaPath.password, { message: 'Password is required' });
+    },
+    {
+      submission: {
+        action: async (field) => {
+          // TODO field().value()
+        },
+        onInvalid: (field) => {
+          // TODO
+        },
+      },
+    },
+  );
+}
